bc215b499d22980a23f73b6ac48fa7119624dc47
LouisLapointe01 <114722726+LouisLapointe01@users.noreply.github.com>
2023-05-05 17:33:32 +0200

    EX3

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -3,16 +3,31 @@ package ilu2;
 public class Welcome {
 
 	public static String welcome(String input) {
+		if (input.isBlank()) {return "Hello, my friend";} //verif chaine vide
 		
 		StringBuilder texte = new StringBuilder("Hello, ");
 		
-		if (input.isBlank()) {return "Hello, my friend";}
-		texte.append(input);
-		if (input.toUpperCase().equals(input)){
-		   texte.append(" !");
-		   return texte.toString().toUpperCase();
+		texte.append(input.substring(0, 1).toUpperCase());
+		texte.append(input.substring(1));
+		
+		if(estEnMajuscules(input)){
+			StringBuilder textemaj = new StringBuilder("HELLO, ");
+			textemaj.append(input);
+			textemaj.append(" !");
+			return textemaj.toString();
 		}
-	
 		return texte.toString();
+		
 	}
+	
+	public static boolean estEnMajuscules(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            char caractere = input.charAt(i);
+
+            if (Character.isLetter(caractere) && !Character.isUpperCase(caractere)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- test/ilu2/WelcomeTest.java
+++ test/ilu2/WelcomeTest.java
@@ -6,12 +6,28 @@ import org.junit.jupiter.api.Test;
 
 class WelcomeTest {
 	
+	@Test
+	void EX1() {
+		System.out.println("Initialisation...");
+		String expected = "Hello, Bob";
+		Welcome.welcome("bob");
+		assertEquals(expected,Welcome.welcome("bob"));
+
+	}
 	
 	@Test
+	void EX2() {
+		String expected = "Hello, my friend";
+		assertEquals(Welcome.welcome("   "),expected);
+		assertEquals(Welcome.welcome(""),expected);
+		assertEquals(Welcome.welcome(" "),expected);
+		assertEquals(Welcome.welcome("  "),expected);
+
+	}
+	@Test
 	void EX3() {
-		assertEquals(Welcome.welcome("MICHEL"), "HELLO, MICHEL !");
-		assertEquals(Welcome.welcome("42"), "HELLO, 42 !");
-		assertEquals(Welcome.welcome("Michel"), "Hello, Michel");
+		String expected = "HELLO, MICHEL !";
+		assertEquals(Welcome.welcome("MICHEL"),expected);
 	}
 
 }
